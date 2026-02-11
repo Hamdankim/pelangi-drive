c605933ea416300135f1ca3a3fa072baeb5cd76f
Hamdan Azizul Hakim <hamdanizul24@gmail.com>
2026-02-11 14:23:24 +0700

    change url be

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const BACKEND_URL = "https://your-backend.onrender.com/upload";
+const BACKEND_URL = "http://127.0.0.1:8000/upload";
 
 const form = document.getElementById("uploadForm");
 const statusDiv = document.getElementById("status");
